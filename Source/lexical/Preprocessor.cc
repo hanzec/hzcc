37bf6f2b6932396650403acb5a93a82ac0ff6348
Hanze Chen <tony.chen424@gmail.com>
2022-02-20 21:06:00 -0600

    code update

--- Source/lexical/Preprocessor.cc
+++ Source/lexical/Preprocessor.cc
@@ -3,7 +3,7 @@
 namespace Mycc::Lexical {
 Status PreprocessSource(std::istream& source, std::ostream& output,
                         const std::string& source_name) {
-  
+  return Status::OkStatus();
 
 }
 }  // namespace Mycc::Lexical
